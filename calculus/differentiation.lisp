45e7d1625c8a41c9eb8ed0dd23c477d3974ab4b8
julian.wolf <julian.wolf@mail.mcgill.ca>
2016-06-08 17:06:26 -0400

    add function to compute the jacobian of an expression and related helper functions; move convert-expression to a toplevel function for use outside of expression->function.

--- calculus/differentiation.lisp
+++ calculus/differentiation.lisp
@@ -100,6 +100,51 @@ values-plist."
             (apply expr-function values-plist))
          epsilon))))
 
-(defun jacobian (expr var)
-  nil)
+(defun %to-var-list (var-plist)
+  "Turn a var-plist of the form (:a 2 :b 5 :c 17) into
+a list of variables of the form (list 'a 'b 'c)."
+  (let ((var-key (car var-plist)))
+    (if (null var-key)
+        nil
+        (cons (intern (symbol-name var-key))
+              (%to-var-list (cddr var-plist))))))
 
+(defun %minimal-sorted-parameter-list (generated-var-list supplied-var-list)
+  "Check that all elements of generated-var-list are present in
+supplied-var-list and remove any extra elements from the latter."
+  (if (loop for generated-var in generated-var-list
+         always (find generated-var supplied-var-list :test #'equal))
+      (loop for supplied-var in supplied-var-list
+         when (find supplied-var generated-var-list :test #'equal)
+         collect it)
+      (error "supplied-var-list does not contain all necessary variables.")))
+
+(defun jacobian (expr var var-values parameter-values-plist)
+  "Construct the jacobian of expr for values of var given
+by var-values with parameters (all arguments to expr except
+var) given by parameter-values-plist."
+  (multiple-value-bind
+        (converted-expression variable-list)
+      (convert-expression expr)
+    (let* ((parameter-list (%minimal-sorted-parameter-list
+                            (remove var variable-list)
+                            (%to-var-list parameter-values-plist)))
+           (m (length var-values))
+           (n (length parameter-list))
+           (jacobian-matrix (make-array (list m n)
+                             :initial-element nil)))
+      (loop for j from 0 below n
+         do (let ((derivative-function
+                    (expression->function
+                     (differentiate-analytic expr (nth j parameter-list)))))
+              (loop for i from 0 below m
+                 do (setf (aref jacobian-matrix i j)
+                          (apply derivative-function
+                                 (append (list (intern (symbol-name var)
+                                                       "KEYWORD")
+                                               (nth i var-values))
+                                         parameter-values-plist))))))
+      jacobian-matrix)))
+                                                   
+                                                   
+                                 
