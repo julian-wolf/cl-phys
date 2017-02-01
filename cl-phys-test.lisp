a7862515d8c24c6c37011741b0cf58075a17390c
Julian Wolf <julian.wolf@mail.mcgill.ca>
2017-02-01 07:26:02 -0800

    update test framework (ish); fix coveralls support maybe

--- cl-phys-test.lisp
+++ cl-phys-test.lisp
@@ -4,15 +4,23 @@
   (:use #:cl
 	#:cl-coveralls
 	#:lisp-unit
-	#:cl-phys))
+	#:cl-phys)
+  (:export :run-all-tests))
 
 (in-package #:cl-phys-test)
+
+(defparameter *print-errors* t)
+(defparameter *print-failures* t)
 
 ;; TODO: tests
 
 (define-test auto-pass
     (assert-equal 5 5))
 
-(let ((*print-errors* t)
-      (*print-failures* t))
-  (run-tests :all :cl-phys-test))
+(defun run-all-tests ()
+  "Runs all tests, returning 0 if they pass and -1 otherwise"
+  (let ((db (run-tests :all :cl-phys-test)))
+    (if (and (null (failed-tests db))
+	     (null (error-tests db)))
+	0
+	-1)))
